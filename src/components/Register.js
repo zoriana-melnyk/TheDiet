0b9be8b4f55a01397256f8d01888c2dfb59a25d5
zoriana-melnyk <ziraolena@gmail.com>
2021-12-07 23:34:56 +0200

    feat: change language

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,28 +4,28 @@ import login from "../img/login.jpg"
 function Register({ onClick }) {
     return (
         <div className="base-constructor" >
-            <div className="header">Register</div>
+            <div className="header">Зареєструватися</div>
             <div className="content">
                 <div className="image">
                     <img src={login} alt="login_img" />
                 </div>
                 <div className="form">
                     <div className="form-group">
-                        <label htmlFor="username">Username</label>
-                        <input type="text" name="username" placeholder="username" />
+                        <label htmlFor="username">Нікнейм</label>
+                        <input type="text" name="username" placeholder="нікнейм" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" name="email" placeholder="email" />
+                        <label htmlFor="email">Eлекронна пошта</label>
+                        <input type="email" name="email" placeholder="електронна пошта" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" placeholder="password" />
+                        <label htmlFor="password">Пароль</label>
+                        <input type="password" name="password" placeholder="пароль" />
                     </div>
                 </div>
             </div>
             <div className="footer">
-                <button type="buttton" className="btn">Register</button>
+                <button type="buttton" className="btn">Зареєструватися</button>
             </div>
         </div>
     );
