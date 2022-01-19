02f49354a16f62e068c45977c49e10885d02d9be
zoriana-melnyk <ziraolena@gmail.com>
2022-01-19 21:15:13 +0200

    feat: modified reviews-component

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,48 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Reviews.scss';
 
-import Man from '../img/man.png';
-import Woman from '../img/woman.png';
-import Boy from '../img/boy.png';
-import MoslemWoman from '../img/moslem-woman.png';
-
-// import { Button } from 'react-bootstrap';
-
+const COMMENTS = [
+    'Чудовий сервіс! Дуже зручний функціонал.',
+    `Клас! Це саме той сайт, що я так довго шукала.
+    Дуже зручно розраховувати калорійність готових страв та складати денний раціон.`,
+    `Сайт зручний, але мінус у тому, що потрібно зареєструватися для того,
+    щоб зберегти створене меню або страву`,
+    `Все добре. Можна легко створити страву та меню. З доставкою  є нюанси.
+    Та, сайт зручний у користування`
+]
 function Reviews() {
+    const [users, setUsers] = useState([]);
+    const getUsers = async () => {
+        const { results } = await (await fetch('https://randomuser.me/api/?results=4')).json();
+        setUsers(results);
+    }
+    useEffect(() => {
+        getUsers();
+    }, [])
     return (
-        <div className="Footer">
+        <div className="Footer-reviews">
             <footer className="Reviews-block">
                 <h3 className="Text-reviews-header p-5">
                     Відгуки наших клієнтів
                 </h3>
-                <div className="Reviews-container p-4">
-                    <img src={Man} className="Reviews-container-img Man mr-4" alt="man_img" />
-                    <h4 className="Reviews-container-header">
-                        Чудовий сервіс! Дуже зручний функціонал.
-                    </h4>
-                </div>
-                <div className="Reviews-container p-4">
-                    <h4 className="Reviews-container-header">
-                        Клас! Це саме той сайт, що я так довго шукала.
-                        Дуже зручно розраховувати калорійність готових страв та складати денний раціон.
-                    </h4>
-                    <img src={Woman} className="Reviews-container-img Woman mr-4" alt="woman_img" />
-                </div>
-                <div className="Reviews-container p-4">
-                    <img src={Boy} className="Reviews-container-img Boy mr-4" alt="boy_img" />
-
-                    <h4 className="Reviews-container-header">
-                        Сайт зручний, але мінус у тому, що потрібно зареєструватися для того,
-                        щоб зберегти створене меню або страву
-                    </h4>
-                </div>
-                <div className="Reviews-container p-4">
-                    <h4 className="Reviews-container-header">
-                        Все добре. Можна легко створити страву та меню. З доставкою  є нюанси.
-                        Та, сайт зручний у користування
-                    </h4>
-                    <img src={MoslemWoman} className="Reviews-container-img Moslem-woman mr-4" alt="moslem-woman_img" />
-                </div>
+                {users.map((user, idx) => {
+                    return <div key={user?.name?.first} className="Reviews-container p-4">
+                        <img src={user?.picture?.medium} alt="img" />
+                        <div className="Reviews-container-content">
+                            <h4>{user?.name?.first} {user?.name?.last}</h4>
+                            <blockquote>{COMMENTS[idx]}</blockquote>
+                        </div>
+                    </div>
+                })}
             </footer>
         </div>
     );
