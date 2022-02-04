76f785406f228b07acbafb1428b126f54868168e
zoriana-melnyk <ziraolena@gmail.com>
2022-02-04 16:17:35 +0200

    feat: added Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import './Contact.scss';
+import socialIcon from '../img/svg_img/social.svg'
+import mailIcon from '../img/svg_img/mail.svg'
+import phoneIcon from '../img/svg_img/phone.svg'
+
+
+import { Button } from 'react-bootstrap';
+
+function Contact() {
+    return (
+        <div className="Contacts">
+            <div className="Contacts__info">
+                <h1 className="Contacts__info__header p-2">
+                    Контакти
+                </h1>
+            </div>
+            <div className="Contacts__navContainer">
+                <div className="Contacts__navContainer__contactsBlock social-media">
+                    <div className="Contacts__navContainer__contactsBlock__infoBlock">
+                        <h2 className="Contacts__navContainer__contactsBlock__infoBlock--text">
+                            Ми - в соцмережах
+                        </h2>
+                        <img src={socialIcon} className="Contacts__navContainer__contactsBlock__infoBlock--img row mx-auto p-2" alt="socialIcon_img" />
+                    </div>
+                    <Button className="Contacts__navContainer__contactsBlock--navButton">
+                        Звʼязатися
+                    </Button>
+                </div>
+                <div className="Contacts__navContainer__contactsBlock e-mail">
+                    <div className="Contacts__navContainer__contactsBlock__infoBlock">
+                        <h2 className="Contacts__navContainer__contactsBlock__infoBlock--text">
+                            Наша електронна скринька
+                        </h2>
+                        <img src={mailIcon} className="Contacts__navContainer__contactsBlock__infoBlock--img row mx-auto p-2" alt="mailIcon_img" />
+                    </div>
+                    <Button className="Contacts__navContainer__contactsBlock--navButton">
+                        Звʼязатися
+                    </Button>
+                </div>
+                <div className="Contacts__navContainer__contactsBlock phone">
+                    <div className="Contacts__navContainer__contactsBlock__infoBlock">
+                        <h2 className="Contacts__navContainer__contactsBlock__infoBlock--text">
+                            Бажаєте зателефонувати?
+                        </h2>
+                        <img src={phoneIcon} className="Contacts__navContainer__contactsBlock__infoBlock--img row mx-auto p-2" alt="phoneIcon_img" />
+                    </div>
+                    <Button className="Contacts__navContainer__contactsBlock--navButton">
+                        Звʼязатися
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+}
+export { Contact };
