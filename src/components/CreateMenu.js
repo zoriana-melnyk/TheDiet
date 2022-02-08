0daa04ec287f3e332ceabf98114eedbb8c1492c6
zoriana-melnyk <ziraolena@gmail.com>
2022-02-08 20:34:28 +0200

    feat: fixed dropdownItem in CreateMenu-component

--- src/components/CreateMenu.js
+++ src/components/CreateMenu.js
@@ -25,55 +25,55 @@ function CreateMenu() {
                         </h3>
                     </div>
                     {/* Menu selectors */}
-                    <div className="Menu-MenuItemn">
-                        <div className="Menu-MenuItemn-row">
+                    <div className="Menu-MenuItem">
+                        <div className="Menu-MenuItem-row">
                             {/* Brekfast */}
-                            <div className="Menu-MenuItemn-row-Brekfast">
-                                <h3 className="Menu-MenuItemn-row-Brekfast-header">
+                            <div className="Menu-MenuItem-row-Brekfast">
+                                <h3 className="Menu-MenuItem-row-Brekfast-header">
                                     Сніданок:
                                 </h3>
-                                {/* Brekfast Itemn 1 */}
-                                <div className="Menu-MenuItemn-row-Brekfast-header-block">
-                                    <h4 className="Menu-MenuItemn-row-Brekfast-header-block-text itemn-1">
+                                {/* Brekfast Item 1 */}
+                                <div className="Menu-MenuItem-row-Brekfast-header-block">
+                                    <h4 className="Menu-MenuItem-row-Brekfast-header-block-text item-1">
                                         Страва:
                                     </h4>
                                     <Dropdown>
-                                        <Dropdown.Toggle className="Menu-MenuItemn-row-Brekfast-header-block-text-toggleItemn" variant="secondary" id="DishItemn">
+                                        <Dropdown.Toggle className="Menu-MenuItem-row-Brekfast-header-block-text-toggleItem" variant="secondary" id="DishItem">
                                             Оберіть страву
                                         </Dropdown.Toggle>
-                                        <Dropdown.Menu className="Menu-MenuItemn-row-Brekfast-header-block-text-toggleItemn-dishItemn">
+                                        <Dropdown.Menu className="Menu-MenuItem-row-Brekfast-header-block-text-toggleItem-dishItem">
                                             <Dropdown.Item href="#/action-1">Тости з авокадо</Dropdown.Item>
                                             <Dropdown.Item href="#/action-2">Сирнички</Dropdown.Item>
                                             <Dropdown.Item href="#/action-3">Яйце пашот</Dropdown.Item>
                                         </Dropdown.Menu>
                                     </Dropdown>
                                 </div>
-                                {/* Breckfast Itemn 2 */}
-                                <div className="Menu-MenuItemn-row-Brekfast-header-block">
-                                    <h4 className="Menu-MenuItemn-row-Brekfast-header-block-text itemn-2">
+                                {/* Breckfast Item 2 */}
+                                <div className="Menu-MenuItem-row-Brekfast-header-block">
+                                    <h4 className="Menu-MenuItem-row-Brekfast-header-block-text item-2">
                                         Страва:
                                     </h4>
                                     <Dropdown>
-                                        <Dropdown.Toggle className="Menu-MenuItemn-row-Brekfast-header-block-text-toggleItemn" variant="secondary" id="DishItemn">
+                                        <Dropdown.Toggle className="Menu-MenuItem-row-Brekfast-header-block-text-toggleItem" variant="secondary" id="DishItem">
                                             Оберіть страву
                                         </Dropdown.Toggle>
-                                        <Dropdown.Menu className="Menu-MenuItemn-row-Brekfast-header-block-text-toggleItemn-dishItemn">
+                                        <Dropdown.Menu className="Menu-MenuItem-row-Brekfast-header-block-text-toggleItem-dishItem">
                                             <Dropdown.Item href="#/action-1">Фруктовий сет</Dropdown.Item>
                                             <Dropdown.Item href="#/action-2">Сирний крем-суп</Dropdown.Item>
                                             <Dropdown.Item href="#/action-3">Омлет</Dropdown.Item>
                                         </Dropdown.Menu>
                                     </Dropdown>
                                 </div>
-                                {/* Brekfast Itemn 3 */}
-                                <div className="Menu-MenuItemn-row-Brekfast-header-block">
-                                    <h4 className="Menu-MenuItemn-row-Brekfast-header-block-text itemn-3">
+                                {/* Brekfast Item 3 */}
+                                <div className="Menu-MenuItem-row-Brekfast-header-block">
+                                    <h4 className="Menu-MenuItem-row-Brekfast-header-block-text item-3">
                                         Страва:
                                     </h4>
                                     <Dropdown>
-                                        <Dropdown.Toggle className="Menu-MenuItemn-row-Brekfast-header-block-text-toggleItemn" variant="secondary" id="DishItemn">
+                                        <Dropdown.Toggle className="Menu-MenuItem-row-Brekfast-header-block-text-toggleItem" variant="secondary" id="DishItem">
                                             Оберіть страву
                                         </Dropdown.Toggle>
-                                        <Dropdown.Menu className="Menu-MenuItemn-row-Brekfast-header-block-text-toggleItemn-dishItemn">
+                                        <Dropdown.Menu className="Menu-MenuItem-row-Brekfast-header-block-text-toggleItem-dishItem">
                                             <Dropdown.Item href="#/action-1">Млинці</Dropdown.Item>
                                             <Dropdown.Item href="#/action-2">Сухофрукти</Dropdown.Item>
                                             <Dropdown.Item href="#/action-3">Вівсянка</Dropdown.Item>
@@ -82,52 +82,52 @@ function CreateMenu() {
                                 </div>
                             </div>
                             {/* Lunch */}
-                            <div className="Menu-MenuItemn-row-Lunch">
-                                <h3 className="Menu-MenuItemn-row-Lunch-header">
+                            <div className="Menu-MenuItem-row-Lunch">
+                                <h3 className="Menu-MenuItem-row-Lunch-header">
                                     Обід:
                                 </h3>
-                                {/* Lunch Itemn 1 */}
-                                <div className="Menu-MenuItemn-row-Lunch-header-block">
-                                    <h4 className="Menu-MenuItemn-row-Lunch-header-block-text itemn-1">
+                                {/* Lunch Item 1 */}
+                                <div className="Menu-MenuItem-row-Lunch-header-block">
+                                    <h4 className="Menu-MenuItem-row-Lunch-header-block-text item-1">
                                         Страва:
                                     </h4>
                                     <Dropdown>
-                                        <Dropdown.Toggle className="Menu-MenuItemn-row-Lunch-header-block-text-toggleItemn" variant="secondary" id="DishItemn">
+                                        <Dropdown.Toggle className="Menu-MenuItem-row-Lunch-header-block-text-toggleItem" variant="secondary" id="DishItem">
                                             Оберіть страву
                                         </Dropdown.Toggle>
-                                        <Dropdown.Menu className="Menu-MenuItemn-row-Lunch-header-block-text-toggleItemn-dishItemn">
+                                        <Dropdown.Menu className="Menu-MenuItem-row-Lunch-header-block-text-toggleItem-dishItem">
                                             <Dropdown.Item href="#/action-1">Запечена форель</Dropdown.Item>
                                             <Dropdown.Item href="#/action-2">Овочі гриль</Dropdown.Item>
                                             <Dropdown.Item href="#/action-3">Сирна нарізка</Dropdown.Item>
                                         </Dropdown.Menu>
                                     </Dropdown>
                                 </div>
-                                {/* Lunch Itemn 2 */}
-                                <div className="Menu-MenuItemn-row-Lunch-header-block">
-                                    <h4 className="Menu-MenuItemn-row-Lunch-header-block-text itemn-1">
+                                {/* Lunch Item 2 */}
+                                <div className="Menu-MenuItem-row-Lunch-header-block">
+                                    <h4 className="Menu-MenuItem-row-Lunch-header-block-text item-1">
                                         Страва:
                                     </h4>
                                     <Dropdown>
-                                        <Dropdown.Toggle className="Menu-MenuItemn-row-Lunch-header-block-text-toggleItemn" variant="secondary" id="DishItemn">
+                                        <Dropdown.Toggle className="Menu-MenuItem-row-Lunch-header-block-text-toggleItem" variant="secondary" id="DishItem">
                                             Оберіть страву
                                         </Dropdown.Toggle>
-                                        <Dropdown.Menu className="Menu-MenuItemn-row-Lunch-header-block-text-toggleItemn-dishItemn">
+                                        <Dropdown.Menu className="Menu-MenuItem-row-Lunch-header-block-text-toggleItem-dishItem">
                                             <Dropdown.Item href="#/action-1">Стейк свинний</Dropdown.Item>
                                             <Dropdown.Item href="#/action-2">Картопля в мундирі</Dropdown.Item>
                                             <Dropdown.Item href="#/action-3">Соус з журавлини</Dropdown.Item>
                                         </Dropdown.Menu>
                                     </Dropdown>
                                 </div>
-                                {/* Lunch Itemn 3 */}
-                                <div className="Menu-MenuItemn-row-Lunch-header-block">
-                                    <h4 className="Menu-MenuItemn-row-Lunch-header-block-text itemn-1">
+                                {/* Lunch Item 3 */}
+                                <div className="Menu-MenuItem-row-Lunch-header-block">
+                                    <h4 className="Menu-MenuItem-row-Lunch-header-block-text item-1">
                                         Страва:
                                     </h4>
                                     <Dropdown>
-                                        <Dropdown.Toggle className="Menu-MenuItemn-row-Lunch-header-block-text-toggleItemn" variant="secondary" id="DishItemn">
+                                        <Dropdown.Toggle className="Menu-MenuItem-row-Lunch-header-block-text-toggleItem" variant="secondary" id="DishItem">
                                             Оберіть страву
                                         </Dropdown.Toggle>
-                                        <Dropdown.Menu className="Menu-MenuItemn-row-Lunch-header-block-text-toggleItemn-dishItemn">
+                                        <Dropdown.Menu className="Menu-MenuItem-row-Lunch-header-block-text-toggleItem-dishItem">
                                             <Dropdown.Item href="#/action-1">Суп овочевий</Dropdown.Item>
                                             <Dropdown.Item href="#/action-2">Хлібна нарізка</Dropdown.Item>
                                             <Dropdown.Item href="#/action-3">Мʼясна нарізка</Dropdown.Item>
@@ -137,52 +137,52 @@ function CreateMenu() {
                             </div>
                         </div>
                         {/* Diner */}
-                        <div className="Menu-MenuItemn-Diner">
-                            <h3 className="Menu-MenuItemn-Diner-header">
+                        <div className="Menu-MenuItem-Diner">
+                            <h3 className="Menu-MenuItem-Diner-header">
                                 Вечеря:
                             </h3>
-                            {/* Diner Itemn 1 */}
-                            <div className="Menu-MenuItemn-Diner-header-block">
-                                <h4 className="Menu-MenuItemn-Diner-header-block-text itemn-1">
+                            {/* Diner Item 1 */}
+                            <div className="Menu-MenuItem-Diner-header-block">
+                                <h4 className="Menu-MenuItem-Diner-header-block-text item-1">
                                     Страва:
                                 </h4>
                                 <Dropdown>
-                                    <Dropdown.Toggle className="Menu-MenuItemn-Diner-header-block-text-toggleItemn" variant="secondary" id="DishItemn">
+                                    <Dropdown.Toggle className="Menu-MenuItem-Diner-header-block-text-toggleItem" variant="secondary" id="DishItem">
                                         Оберіть страву
                                     </Dropdown.Toggle>
-                                    <Dropdown.Menu className="Menu-MenuItemn-Diner-header-block-text-toggleItemn-dishItemn">
+                                    <Dropdown.Menu className="Menu-MenuItem-Diner-header-block-text-toggleItem-dishItem">
                                         <Dropdown.Item href="#/action-1">Сирна нарізка</Dropdown.Item>
                                         <Dropdown.Item href="#/action-2">Запечені овочі</Dropdown.Item>
                                         <Dropdown.Item href="#/action-3">Качка по пекінськи</Dropdown.Item>
                                     </Dropdown.Menu>
                                 </Dropdown>
                             </div>
-                            {/* Diner Itemn 2 */}
-                            <div className="Menu-MenuItemn-Diner-header-block">
-                                <h4 className="Menu-MenuItemn-Diner-header-block-text itemn-1">
+                            {/* Diner Item 2 */}
+                            <div className="Menu-MenuItem-Diner-header-block">
+                                <h4 className="Menu-MenuItem-Diner-header-block-text item-1">
                                     Страва:
                                 </h4>
                                 <Dropdown>
-                                    <Dropdown.Toggle className="Menu-MenuItemn-Diner-header-block-text-toggleItemn" variant="secondary" id="DishItemn">
+                                    <Dropdown.Toggle className="Menu-MenuItem-Diner-header-block-text-toggleItem" variant="secondary" id="DishItem">
                                         Оберіть страву
                                     </Dropdown.Toggle>
-                                    <Dropdown.Menu className="Menu-MenuItemn-Diner-header-block-text-toggleItemn-dishItemn">
+                                    <Dropdown.Menu className="Menu-MenuItem-Diner-header-block-text-toggleItem-dishItem">
                                         <Dropdown.Item href="#/action-1">Паста болоньєзе</Dropdown.Item>
                                         <Dropdown.Item href="#/action-2">Сік фруктовий</Dropdown.Item>
                                         <Dropdown.Item href="#/action-3">Овочі свіжі</Dropdown.Item>
                                     </Dropdown.Menu>
                                 </Dropdown>
                             </div>
-                            {/* Diner Itemn 3 */}
-                            <div className="Menu-MenuItemn-Diner-header-block">
-                                <h4 className="Menu-MenuItemn-Diner-header-block-text itemn-1">
+                            {/* Diner Item 3 */}
+                            <div className="Menu-MenuItem-Diner-header-block">
+                                <h4 className="Menu-MenuItem-Diner-header-block-text item-1">
                                     Страва:
                                 </h4>
                                 <Dropdown>
-                                    <Dropdown.Toggle className="Menu-MenuItemn-Diner-header-block-text-toggleItemn" variant="secondary" id="DishItemn">
+                                    <Dropdown.Toggle className="Menu-MenuItem-Diner-header-block-text-toggleItem" variant="secondary" id="DishItem">
                                         Оберіть страву
                                     </Dropdown.Toggle>
-                                    <Dropdown.Menu className="Menu-MenuItemn-Diner-header-block-text-toggleItemn-dishItemn">
+                                    <Dropdown.Menu className="Menu-MenuItem-Diner-header-block-text-toggleItem-dishItem">
                                         <Dropdown.Item href="#/action-1">Равіолі з індичкою</Dropdown.Item>
                                         <Dropdown.Item href="#/action-2">Соус песто</Dropdown.Item>
                                         <Dropdown.Item href="#/action-3">Чай фруктовий</Dropdown.Item>
