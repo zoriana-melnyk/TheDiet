1e7eccbe0711b5cef61612853e4b9d0167ad6efc
zoriana-melnyk <ziraolena@gmail.com>
2022-02-19 18:31:00 +0200

    feat: changed info-table about caloric of product

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,22 +1,53 @@
 import React from "react";
 import { Table } from "react-bootstrap";
 
+import Select from 'react-select';
+
 import './Recipe.scss';
 
+const cookingOptions = [
+    {
+        label: 'Варити у воді',
+        value: 1.1
+    },
+    {
+        label: 'Варити на пару',
+        value: 1.07
+    },
+    {
+        label: 'Смажити',
+        value: .98
+    },
+    {
+        label: 'Запікати',
+        value: .984
+    },
+    {
+        label: 'Сирий',
+        value: 1
+    }
+];
+const defaultOptionIndex = 2;
+
 function Recipe(props) {
+    const { recipe, onCookingOptionChange } = props;
 
-    // const [showResult, setShowResult] = useState([]);
-
-    // const calculation = (e) => {
-    //     e.preventDefault();
-    //     // const isFormValid = formValidation(); // boolean
-    //     // if (!isFormValid) {
-    //     //     return;
-    //     // }
-    //     const createdRecipe = props.find((prod, idx) => prod.kll === Number(formValues.kll));
-    //     setShowResult([...showResult, { ...formValues, ...createdRecipe }]);
-    // }
-
+    const weight = props.recipe.reduce(
+        (acc, el) => { return acc + el.weight },
+        0
+    );
+    const fullKll = props.recipe.reduce(
+        (acc, el) => { return acc + el.fullKll },
+        0
+    );
+    const kll = props.recipe.reduce(
+        (acc, el) => { return acc + el.kll },
+        0
+    );
+    const dependedKll = props.recipe.reduce(
+        (acc, el) => { return acc + el.cookKll },
+        0
+    );
 
     return (
         <div className="Recipe">
@@ -26,39 +57,38 @@ function Recipe(props) {
                         <tr>
                             <th>Продукт</th>
                             <th>Вага</th>
-                            <th>Калорійність за 100г</th>
+                            <th>kll/100g</th>
                             <th>Калорійність</th>
+                            <th>kll/100g приготування</th>
+                            <th>спосіб приготування</th>
                         </tr>
                     </thead>
                     <tbody className="Recipe__tableContainer__table__body">
-                        {props.recipe.map((prod, idx) => (
+                        {recipe.map((prod, idx) => (
                             <tr key={`product-${idx}`}>
                                 <td>{prod.label}</td>
                                 <td>{`${prod.weight} г`}</td>
                                 <td>{prod.fullKll}</td>
-                                <td>{prod.kll}</td>
+                                <td>{Math.round(prod.kll)}</td>
+                                <td>{Math.round(prod.cookKll)}</td>
+                                <td>
+                                    <Select
+                                        defaultValue={cookingOptions[defaultOptionIndex]}
+                                        options={cookingOptions}
+                                        onChange={selectedOption => {
+                                            onCookingOptionChange(idx, selectedOption)
+                                        }}
+                                    />
+                                </td>
                             </tr>
-
                         ))}
                         <tr >
                             <td>Загальна цінність</td>
-                            <td>{`${props.recipe.reduce(
-                                (acc, el) => { return acc + el.weight },
-                                0
-                            )} г`}</td>
-                            <td>{
-                                `${props.recipe.reduce(
-                                    (acc, el) => { return acc + el.fullKll },
-                                    0
-                                )} клл`
-                            }</td>
-                            <td>{
-                                `${props.recipe.reduce(
-                                    (acc, el) => { return acc + el.kll },
-                                    0
-                                )} клл`
-                            }</td>
-
+                            <td>{`${weight} г`}</td>
+                            <td>{`${fullKll} клл`}</td>
+                            <td>{`${Math.round(kll)} клл`}</td>
+                            <td>{`${Math.round(dependedKll)} клл`}</td>
+                            <td></td>
                         </tr>
                     </tbody>
                 </Table>
@@ -67,4 +97,4 @@ function Recipe(props) {
     )
 
 }
-export { Recipe }
+export { Recipe, cookingOptions, defaultOptionIndex }
