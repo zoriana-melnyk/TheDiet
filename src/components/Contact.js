f4d0271d59f0793510fece9b226c3f4cdf052dca
zoriana-melnyk <ziraolena@gmail.com>
2022-04-27 20:34:47 +0300

    feat: added localization to ContactComponent

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,46 +8,52 @@ import phoneIcon from '../img/svg_img/phone.svg'
 
 import { Button } from 'react-bootstrap';
 
+import { useLittera } from "@assembless/react-littera";
+import { contactTranslations } from "../ContactTranslation";
+
 function Contact() {
+
+    const translated = useLittera(contactTranslations);
+
     return (
         <div id="Contacts" className="Contacts">
             <div className="Contacts__info">
                 <h1 className="Contacts__info__header p-2">
-                    Контакти
+                    {translated.header}
                 </h1>
             </div>
             <div className="Contacts__navContainer">
                 <div className="Contacts__navContainer__contactsBlock social-media">
                     <div className="Contacts__navContainer__contactsBlock__infoBlock">
                         <h2 className="Contacts__navContainer__contactsBlock__infoBlock--text">
-                            Ми - в соцмережах
+                            {translated.social}
                         </h2>
                         <img src={socialIcon} className="Contacts__navContainer__contactsBlock__infoBlock--img row mx-auto p-2" alt="socialIcon_img" />
                     </div>
                     <Button className="Contacts__navContainer__contactsBlock__navButton">
-                        Звʼязатися
+                        {translated.action}
                     </Button>
                 </div>
                 <div className="Contacts__navContainer__contactsBlock e-mail">
                     <div className="Contacts__navContainer__contactsBlock__infoBlock">
                         <h2 className="Contacts__navContainer__contactsBlock__infoBlock--text">
-                            Наша електронна скринька
+                            {translated.mail}
                         </h2>
                         <img src={mailIcon} className="Contacts__navContainer__contactsBlock__infoBlock--img row mx-auto p-2" alt="mailIcon_img" />
                     </div>
                     <Button className="Contacts__navContainer__contactsBlock__navButton">
-                        Звʼязатися
+                        {translated.action}
                     </Button>
                 </div>
                 <div className="Contacts__navContainer__contactsBlock phone">
                     <div className="Contacts__navContainer__contactsBlock__infoBlock">
                         <h2 className="Contacts__navContainer__contactsBlock__infoBlock--text">
-                            Бажаєте зателефонувати?
+                            {translated.phone}
                         </h2>
                         <img src={phoneIcon} className="Contacts__navContainer__contactsBlock__infoBlock--img row mx-auto p-2" alt="phoneIcon_img" />
                     </div>
                     <Button type="button" className="Contacts__navContainer__contactsBlock__navButton">
-                        Звʼязатися
+                        {translated.action}
                     </Button>
                 </div>
             </div>
