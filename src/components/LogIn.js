2fedb8625da8eb979fc9c7dbd6d38d31bb1c5a18
zoriana-melnyk <ziraolena@gmail.com>
2022-04-28 20:37:20 +0300

    feat: added localization to Authorization

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,27 +1,34 @@
 import React from "react";
 import login from "../img/login.jpg"
 
+import { useLittera } from "@assembless/react-littera";
+import { loginTranslations } from "../LoginTranslation";
+
+
 function LogIn({ onClick }) {
+
+    const translated = useLittera(loginTranslations);
+
     return (
         <div className="base-constructor">
-            <div className="header">Увійти</div>
+            <div className="header">{translated.header}</div>
             <div className="content">
                 <div className="image">
                     <img src={login} alt="login-img" />
                 </div>
                 <div className="form">
                     <div className="form-group">
-                        <label htmlFor="username">Нікнейм</label>
+                        <label htmlFor="username">{translated.username}</label>
                         <input type="text" name="username" placeholder="нікнейм" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="password">Пароль</label>
+                        <label htmlFor="password">{translated.password}</label>
                         <input type="password" name="password" placeholder="пароль" />
                     </div>
                 </div>
             </div>
             <div className="footer">
-                <button type="buttton" className="btn">Увійти</button>
+                <button type="buttton" className="btn">{translated.header}</button>
             </div>
         </div>
     );
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,31 +1,37 @@
 import React from "react";
 import login from "../img/login.jpg"
 
+import { useLittera } from "@assembless/react-littera";
+import { registerTranslations } from "../RegisterTranslation";
+
 function Register({ onClick }) {
+
+    const translated = useLittera(registerTranslations);
+
     return (
         <div className="base-constructor" >
-            <div className="header">Зареєструватися</div>
+            <div className="header">{translated.header}</div>
             <div className="content">
                 <div className="image">
                     <img src={login} alt="login_img" />
                 </div>
                 <div className="form">
                     <div className="form-group">
-                        <label htmlFor="username">Нікнейм</label>
+                        <label htmlFor="username">{translated.username}</label>
                         <input type="text" name="username" placeholder="нікнейм" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="email">Eлекронна пошта</label>
+                        <label htmlFor="email">{translated.email}</label>
                         <input type="email" name="email" placeholder="електронна пошта" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="password">Пароль</label>
+                        <label htmlFor="password">{translated.password}</label>
                         <input type="password" name="password" placeholder="пароль" />
                     </div>
                 </div>
             </div>
             <div className="footer">
-                <button type="buttton" className="btn">Зареєструватися</button>
+                <button type="buttton" className="btn">{translated.header}</button>
             </div>
         </div>
     );
