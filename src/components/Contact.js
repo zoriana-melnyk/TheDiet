ed72c795a511d38ba41d490a85b91ea051fff898
zoriana-melnyk <ziraolena@gmail.com>
2022-05-01 14:06:13 +0300

    feat: added feedback on the Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,6 @@ import socialIcon from '../img/svg_img/social.svg'
 import mailIcon from '../img/svg_img/mail.svg'
 import phoneIcon from '../img/svg_img/phone.svg'
 
-
 import { Button } from 'react-bootstrap';
 
 import { useLittera } from "@assembless/react-littera";
@@ -14,6 +13,18 @@ import { contactTranslations } from "../ContactTranslation";
 function Contact() {
 
     const translated = useLittera(contactTranslations);
+
+    const socialHandleClick = () => {
+        window.open("https://t.me/theDietLife");
+    };
+
+    const mailHandleClick = () => {
+        window.open('mailto:thedietlive@gmail.com');
+    };
+
+    const callHandleClick = () => {
+        window.open('tel:900300400');
+    };
 
     return (
         <div id="Contacts" className="Contacts">
@@ -30,7 +41,8 @@ function Contact() {
                         </h2>
                         <img src={socialIcon} className="Contacts__navContainer__contactsBlock__infoBlock--img row mx-auto p-2" alt="socialIcon_img" />
                     </div>
-                    <Button className="Contacts__navContainer__contactsBlock__navButton">
+                    <Button className="Contacts__navContainer__contactsBlock__navButton"
+                        onClick={socialHandleClick}>
                         {translated.action}
                     </Button>
                 </div>
@@ -41,7 +53,8 @@ function Contact() {
                         </h2>
                         <img src={mailIcon} className="Contacts__navContainer__contactsBlock__infoBlock--img row mx-auto p-2" alt="mailIcon_img" />
                     </div>
-                    <Button className="Contacts__navContainer__contactsBlock__navButton">
+                    <Button className="Contacts__navContainer__contactsBlock__navButton"
+                        onClick={mailHandleClick}>
                         {translated.action}
                     </Button>
                 </div>
@@ -52,7 +65,8 @@ function Contact() {
                         </h2>
                         <img src={phoneIcon} className="Contacts__navContainer__contactsBlock__infoBlock--img row mx-auto p-2" alt="phoneIcon_img" />
                     </div>
-                    <Button type="button" className="Contacts__navContainer__contactsBlock__navButton">
+                    <Button type="button" className="Contacts__navContainer__contactsBlock__navButton"
+                        onClick={callHandleClick}>
                         {translated.action}
                     </Button>
                 </div>
